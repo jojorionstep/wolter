a5b7107c862eb35476edc8a73afb3a1d8648a567
perso7 <kue.gamer@gmail.com>
2017-12-24 12:35:31 +0900

    sample

---
+++ js/source/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Logo from './components/Logo';
+
+ReactDOM.render(
+    <h1>
+	<Logo /> Welcom to my applicattion!!
+    </h1>,
+    document.getElementById('app')
+);
+
+
+
